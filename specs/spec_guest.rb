80b30651dd3eebd560faf51c07ee4c31467d00ba
Ralph Burden <ralphburden@hotmail.com>
2019-04-12 13:58:12 +0100

    add guest class with test for guest name

---
+++ specs/spec_guest.rb
@@ -0,0 +1,18 @@
+require("minitest/autorun")
+require("minitest/rg")
+
+require_relative("../guest.rb")
+
+class GuestTest < MiniTest::Test
+
+  def setup
+    @guest1 = Guest.new("Alex")
+    @guest2 = Guest.new("John")
+  end #end setup
+
+  def test_guest_has_name
+    assert_equal("Alex", @guest1.name)
+  end
+
+
+end #end class
